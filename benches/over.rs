647f644185121db71684a3cc5d1a2f1475b86ac9
Doug Lau <doug.p.lau@gmail.com>
2019-04-07 12:53:08 -0500

    Added "over" benchmark.

---
+++ benches/over.rs
@@ -0,0 +1,59 @@
+#[macro_use]
+extern crate criterion;
+extern crate pix;
+
+use pix::*;
+use criterion::Criterion;
+
+fn gray_over(c: &mut Criterion, sz: u32) {
+    let s = format!("gray_over_{}", sz);
+    c.bench_function(&s, move |b| {
+        let mut r = Raster::<Gray8>::new(sz, sz);
+        let mut m = Raster::<Alpha8>::new(sz, sz);
+        m.set_pixel(0, 0, Alpha8::new(255));
+        m.set_pixel(sz - 1, sz - 1, Alpha8::new(128));
+        let mask = m.as_u8_slice();
+        b.iter(|| r.over(mask, Gray8::new(100)))
+    });
+}
+
+fn gray_over_16(c: &mut Criterion) {
+    gray_over(c, 16);
+}
+
+fn gray_over_256(c: &mut Criterion) {
+    gray_over(c, 256);
+}
+
+fn gray_over_512(c: &mut Criterion) {
+    gray_over(c, 512);
+}
+
+fn rgba_over(c: &mut Criterion, sz: u32) {
+    let s = format!("rgba_over_{}", sz);
+    c.bench_function(&s, move |b| {
+        let mut r = Raster::<Rgba8>::new(sz, sz);
+        let mut m = Raster::<Alpha8>::new(sz, sz);
+        m.set_pixel(0, 0, Alpha8::new(255));
+        m.set_pixel(sz - 1, sz - 1, Alpha8::new(128));
+        let mask = m.as_u8_slice();
+        b.iter(|| r.over(mask, Rgba8::new(100, 50, 150, 255)))
+    });
+}
+
+fn rgba_over_16(c: &mut Criterion) {
+    rgba_over(c, 16);
+}
+
+fn rgba_over_256(c: &mut Criterion) {
+    rgba_over(c, 256);
+}
+
+fn rgba_over_512(c: &mut Criterion) {
+    rgba_over(c, 512);
+}
+
+criterion_group!(benches, gray_over_16, gray_over_256, gray_over_512,
+                          rgba_over_16, rgba_over_256, rgba_over_512);
+
+criterion_main!(benches);
